1354335b027ed663880cdb9596a06a76b67c99bc
saarc <saarc@example.com>
2019-07-26 17:48:25 +0900

    새 파일:       network/docker-compose-template.yml 	수정함:        network/docker-compose.yml 	수정함:        network/start.sh

--- network/start.sh
+++ network/start.sh
@@ -7,10 +7,32 @@
 # Exit on first error, print all commands.
 set -ev
 
-# don't rewrite paths for Windows Git Bash users
-export MSYS_NO_PATHCONV=1
+function replacePrivateKey() {
+    echo "ca key file exchange"
+    cp docker-compose-template.yml docker-compose.yml
+    PRIV_KEY=$(ls crypto-config/peerOrganizations/org1.example.com/ca/ | grep _sk)
+    sed -i "s/CA_PRIVATE_KEY/${PRIV_KEY}/g" docker-compose.yml
+}
+
+function checkPrereqs() {
+    # check config dir
+    if [ ! -d "crypto-config" ]; then
+        echo "crypto-config dir missing"
+        exit 1
+    fi
+    # check crypto-config dir
+     if [ ! -d "config" ]; then
+        echo "config dir missing"
+        exit 1
+    fi
+}
+
+checkPrereqs
+replacePrivateKey
 
 docker-compose -f docker-compose.yml down
+
+replacePrivateKey
 
 docker-compose -f docker-compose.yml up -d ca.example.com orderer.example.com couchdb1 couchdb2 couchdb3 peer0.org1.example.com  peer0.org2.example.com peer0.org3.example.com cli
 docker ps -a
@@ -25,7 +47,10 @@ sleep ${FABRIC_START_TIMEOUT}
 docker exec cli peer channel create -o orderer.example.com:7050 -c mychannel -f /etc/hyperledger/configtx/channel.tx
 # Join peer0.org1.example.com to the channel.
 docker exec -e "CORE_PEER_LOCALMSPID=Org1MSP" -e "CORE_PEER_MSPCONFIGPATH=/etc/hyperledger/msp/users/Admin@org1.example.com/msp" peer0.org1.example.com peer channel join -b /etc/hyperledger/configtx/mychannel.block
-
+sleep 5
+# Join peer0.org2.example.com to the channel.
 docker exec -e "CORE_PEER_LOCALMSPID=Org2MSP" -e "CORE_PEER_MSPCONFIGPATH=/etc/hyperledger/msp/users/Admin@org2.example.com/msp" peer0.org2.example.com peer channel join -b /etc/hyperledger/configtx/mychannel.block
-
+sleep 5
+# Join peer0.org2.example.com to the channel.
 docker exec -e "CORE_PEER_LOCALMSPID=Org3MSP" -e "CORE_PEER_MSPCONFIGPATH=/etc/hyperledger/msp/users/Admin@org3.example.com/msp" peer0.org3.example.com peer channel join -b /etc/hyperledger/configtx/mychannel.block
+sleep 5
