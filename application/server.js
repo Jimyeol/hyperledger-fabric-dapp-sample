8fcfe076d0cff2371ad2d827ee841ab644ad88bf
saarc <saarc99@naver.com>
2019-07-31 14:12:57 +0900

    Update server.js

--- application/server.js
+++ application/server.js
@@ -11,7 +11,7 @@ const HOST = '0.0.0.0';
 const { FileSystemWallet, Gateway } = require('fabric-network');
 const fs = require('fs');
 const path = require('path');
-const ccpPath = path.resolve(__dirname, '..','connection.json');
+const ccpPath = path.resolve(__dirname, '..', 'network', ,'connection.json');
 const ccpJSON = fs.readFileSync(ccpPath, 'utf8');
 const ccp = JSON.parse(ccpJSON);
 
